501d86a7559a1ce59bc0f0693fcb0edbc31b31b3
Abdullah Bahsrat <abdullahbasharat400@gmail.com>
2025-03-05 12:42:54 +0500

    Add Serializer Field in serializers

--- WATCHMATE/watchlist_app/api/serializers.py
+++ WATCHMATE/watchlist_app/api/serializers.py
@@ -7,31 +7,58 @@ def validate_obj(value):
         raise serializers.ValidationError("Movie is too short")
     return value
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField(validators=[validate_obj])
-    active = serializers.BooleanField()
-    
-    def create(self, validated_data):
-        """
-        Create and return a new `Movie` instance, given the validated data.
-        :param validated_data:
-        :return:
-        """
-        return Movie.objects.create(**validated_data)
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField(validators=[validate_obj])
+#     active = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         """
+#         Create and return a new `Movie` instance, given the validated data.
+#         :param validated_data:
+#         :return:
+#         """
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         """
+#         Update and return an existing `Movie` instance, given the validated data.
+#         """
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     #Field Level Validators
+#     def validate_name(self,value):
+#         if len(value) < 3:
+#             raise serializers.ValidationError('Name must be at least 3 characters')
+#         return value
+#
+#     #Object Level Validaotrs
+#     def validate(self,data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError('Name and Description must be different')
+#         return data
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Movie` instance, given the validated data.
-        """
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
 
-    #Field Level Validators
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        # fields = '__all__'
+        # fields = ['name','description']         #Fields to display
+        exclude = ['id']
+
+    def get_len_name(self, obj):
+        return len(obj.name)
+
+
+
+    # Field Level Validators
     def validate_name(self,value):
         if len(value) < 3:
             raise serializers.ValidationError('Name must be at least 3 characters')
